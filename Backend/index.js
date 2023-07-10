4a950faa6e84bc202e186ce52df28938c0f43d05
Deepkamal yadav <dkits1995@gmail.com>
2023-07-10 19:02:34 +0530

    backend auth

---
+++ Backend/index.js
@@ -0,0 +1,23 @@
+const cookieSession = require("cookie-session");
+const express = require("express");
+const passport = require("passport");
+const cors = require("cors");
+const app = express();
+
+app.use(
+  cookieSession({ name: "session", keys: ["Deep"], maxAge: 24 * 60 * 60 * 100 })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET, HEAD,POST, PUT, DELETE",
+    Credentials: true,
+  })
+);
+
+app.listen("5000", () => {
+  console.log("listening on port 5000");
+});
